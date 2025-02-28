95873b864c44b0a871d42a4f3d12d95367ef42b9
cleberrobertodg <cleberrobertodg@gmail.com>
2025-02-28 16:06:36 -0300

    fix banner text

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,7 +1,12 @@
 <template>
     <div id="main-banner">
-        <h1>Make Your Burger</h1>
-    </div>
+    <video ref="backgroundVideo" autoplay muted playsinline onloadedmetadata="this.playbackRate=0.5;">
+      <source src="https://v.ftcdn.net/09/19/72/30/700_F_919723081_8X6YRQyBRZjU3XPLTHWFIATMWMLWdrVD_ST.mp4" type="video/mp4" />
+      Seu navegador não suporta vídeos em HTML5.
+    </video>
+    <div class="overlay"></div>
+    <h1>Make Your Burguer</h1>
+  </div>
 </template>
 
 <script>
@@ -10,25 +15,44 @@
 
 <style scoped>
     #main-banner{
-        background-image: url('https://raw.githubusercontent.com/matheusbattisti/curso_vue_yt/refs/heads/main/18_projeto/public/img/burger.jpg');
-        background-position: 0 -250px;
-        background-size: cover;
-        height: 800px;
+        position: relative;
+        width: 100%;
+        height: 100vh;
         display: flex;
         align-items: center;
-        justify-content: flex-start;
+        justify-content: center;
+        overflow: hidden;
     }
 
     #main-banner h1{
+        
+        position: relative;
         color: #fff;
+        font-size: 4vw;
         text-align: center;
-        font-size: 80px;
-        padding: 20px 40px;
-        transition: .5s;
+        background-color: rgba(0, 0, 0, 0.8); 
+        padding: 15px 30px;
+        border-radius: 10px;
+        z-index: 2;
     }
 
-    #main-banner h1:hover {
-        color: #FCBA03;
-        background-color: transparent;
-    }
+    #main-banner video {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    z-index: 0;
+  }
+    #main-banner .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.3);
+    z-index: 1;
+  }
 </style>
